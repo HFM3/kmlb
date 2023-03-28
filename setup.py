25a58f60794784e816d060e35bf6ebbadb2e7925
HFM3 <24279380+HFM3@users.noreply.github.com>
2023-03-28 13:37:09 -0400

    V: 0.0.84

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="kmlb",
-    version="0.0.82",
+    version="0.0.84",
     author="HFM3",
     description="A Straightforward Google Earth KML Builder",
     long_description=long_description,
